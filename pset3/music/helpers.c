7ebef22aa4daf457eb4b4038e07e267c84a9f574
Michael DAntuono <mikeishere3@gmail.com>
2018-04-24 16:41:12 +0000

    music completed

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -17,7 +17,7 @@ int duration(string fraction)
 {
     x = atoi(&fraction[0]);
     y = atoi(&fraction[2]);
-    printf("x = %i and y = %i", x, y);
+    // printf("x = %i and y = %i", x, y);
 
     switch (y)
     {
@@ -39,7 +39,7 @@ int frequency(string note)
     char noteVal = note[0];
     char accidental = ' ';
     int octave;
-    int acc;
+    double freqVar = 440.0; // Starting frequency of A4
     int freq;
 
     if (strlen(note) == 3)
@@ -51,19 +51,73 @@ int frequency(string note)
     {
         octave = atoi(&note[1]);
     }
-    printf("Note is %s, note value is %c, accidental is %c, octave is %i\n", note, noteVal, accidental, octave);
+    // printf("Note is %s, note value is %c, accidental is %c, octave is %i\n", note, noteVal, accidental, octave);
 
     switch (accidental)
     {
-        case # :
-            acc = round(440 * 2 * (2 * (1/12)); // A4 added with the accidental
+        case '#' :
+            freqVar = freqVar * pow(2.0, 1.0 / 12.0); // A4 added with the accidental
             break;
-        case b :
-            acc = round(440 / 2 * (2 * (1/12)); // A4 added with the accidental
+        case 'b' :
+            freqVar = freqVar / pow(2.0, 1.0 / 12.0); // A4 added with the accidental
+            break;
+        default :
+            // freqVar = 440;
+            break;
+
+    }
+    switch (octave)
+    {
+        case 1 :
+            freqVar = freqVar / 8.0;
+            break;
+        case 2 :
+            freqVar = round(freqVar / 4.0);
+            break;
+        case 3 :
+            freqVar = freqVar / 2.0;
+            break;
+        case 4 :
+            break;
+        case 5 :
+            freqVar = freqVar * 2.0;
+            break;
+        case 6 :
+            freqVar = freqVar * 4.0;
+            break;
+        case 7 :
+            freqVar = freqVar * 8.0;
+            break;
+        case 8 :
+            freqVar = freqVar * 16.0;
             break;
     }
 
-    freq =
+    switch (noteVal)
+    {
+        case 'C' :
+            freqVar = freqVar / pow(2.0, 9.0 / 12.0);
+            break;
+        case 'D' :
+            freqVar = freqVar / pow(2.0, 7.0 / 12.0);
+            break;
+        case 'E' :
+            freqVar = freqVar / pow(2.0, 5.0 / 12.0);
+            break;
+        case 'F' :
+            freqVar = freqVar / pow(2.0, 4.0 / 12.0);
+            break;
+        case 'G' :
+            freqVar = freqVar / pow(2.0, 2.0 / 12.0);
+            break;
+        case 'A' :
+            break;
+        case 'B' :
+            freqVar = freqVar * pow(2.0, 2.0 / 12.0);
+            break;
+    }
+
+    freq = round(freqVar);
 
     return freq; // implement the math of accidentals, then octaves, then note
 }
@@ -71,7 +125,7 @@ int frequency(string note)
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    if (!s) // If there is no string...
+    if (strlen(s) == 0) // If there is no string...
     {
         return true;
     }
