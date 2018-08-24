d6d8fcf1d41f6d26f06b4c3c79959bcf024f561e
Michael DAntuono <mikeishere3@gmail.com>
2018-08-24 20:27:13 +0000

    similarities finished

--- pset6/similarities/helpers.py
+++ pset6/similarities/helpers.py
@@ -16,27 +16,26 @@ def sentences(a, b):
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
-    # extract substrings
-    aSubstrings = []
-    bSubstrings = []
-    dup_items = set()
-    matchingSubstrings = []
 
     if n > len(a) or n > len(b):
         return []
-
+    # extract substrings
     # Iterate through a and b and append all substrings of length n to lists
-    for i in range(len(a)):
+    aSubstrings = []
+    for i in range(len(a) - n + 1):
         aSubstrings.append(a[i:i + n])
-    for i in range(len(b)):
-        bSubstrings.append(b[i:i + n])
+    bSubstrings = []
+    for j in range(len(b) - n + 1):
+        bSubstrings.append(b[j:j + n])
 
-    tempSubstrings = list(set(aSubstrings).intersection(bSubstrings))
+    return set(aSubstrings) & set(bSubstrings)
 
-    # Check for duplicates
-    for x in tempSubstrings:
-        if x not in dup_items:
-            matchingSubstrings.append(x)
-            dup_items.add(x)
-
-    return matchingSubstrings
+# Check for duplicates
+# def checkDup(tempList):
+#     dup_items = set()
+#     nonDuplicates = []
+#     for x in tempList:
+#         if x not in dup_items:
+#             nonDuplicates.append(x)
+#             dup_items.add(x)
+#     return nonDuplicates
